6f72742cb3737107650cce1857abbde8cc38be35
HosamHegly <hossam_one@hotmail.com>
2024-03-26 00:25:37 +0200

    fixed test runner not finding serial

--- parallel_test_runner_sample.py
+++ parallel_test_runner_sample.py
@@ -11,33 +11,30 @@ def run_pytest(parallel=False):
     # Load configuration
     config = get_config_data()
 
-    # Directory where all tests are located
     ui_tests_path = "tests/api"
-    # Reports directory
     reports_dir = "reports"
-    # Ensure reports directory exists
     os.makedirs(reports_dir, exist_ok=True)
 
-    # Adjust the path to your virtual environment's Python executable as needed
     python_path = os.path.join("venv", "Scripts", "python.exe")
 
     # Base command using the virtual environment's Python
     base_cmd = [python_path, "-m", "pytest", ui_tests_path]
 
-    # HTML report file path
     html_report = os.path.join(reports_dir, "report.html")
 
-    # Command for parallel execution
     if parallel:
         parallel_cmd = base_cmd + ["-n", "3", "-m", "not serial", f"--html={html_report}"]
         subprocess.run(parallel_cmd, check=True)
-
-        # Command for running serial tests
+    try:
         serial_html_report = os.path.join(reports_dir, "report_serial.html")
         serial_cmd = base_cmd + ["-m", "serial", f"--html={serial_html_report}"]
         subprocess.run(serial_cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 5:  # No tests were collected
+            print("No serial tests were found.")
+        else:
+            raise
     else:
-        # Command for non-parallel execution (all tests, including serial)
         non_parallel_cmd = base_cmd + [f"--html={html_report}"]
         subprocess.run(non_parallel_cmd, check=True)
 
