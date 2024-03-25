5259c23793451f80fbec7c60c53a228f822a6d3c
HosamHegly <hossam_one@hotmail.com>
2024-03-25 23:30:29 +0200

    adjusted jenkisfile and test runner

--- parallel_test_runner_sample.py
+++ parallel_test_runner_sample.py
@@ -1,28 +1,35 @@
-import os
 import subprocess
 from Utils.json_reader import get_config_data
+
 
 def run_pytest(parallel=False):
     # Directory where all tests are located
     ui_tests_path = "tests/api"
 
+    # Reports directory
     reports_dir = "reports"
+    # Ensure reports directory exists
+    os.makedirs(reports_dir, exist_ok=True)
 
-    cmd = ["pytest", ui_tests_path, f"--html={reports_dir}/report.html"]
+    # Path to the Python executable within the virtual environment
+    venv_python = "venv/Scripts/python.exe"
+
+    # Constructing pytest command
+    pytest_cmd = f"{venv_python} -m pytest {ui_tests_path} --html={reports_dir}/report.html"
 
     if parallel:
         # Runs all tests except those marked as 'serial'
-        cmd.extend(["-n", "3", "-m", "not serial"])
-        subprocess.run(cmd)
+        pytest_cmd += " -n 3 -m not serial"
+        subprocess.run(pytest_cmd, shell=True)
 
-        cmd = ["pytest", ui_tests_path, "-m", "serial", f"--html={reports_dir}/report_serial.html"]
+        # Now run the serial tests without xdist
+        pytest_cmd = f"{venv_python} -m pytest {ui_tests_path} -m serial --html={reports_dir}/report_serial.html"
     else:
-        cmd = ["pytest", ui_tests_path, f"--html={reports_dir}/report_serial.html"]
-
-    os.makedirs(reports_dir, exist_ok=True)
+        pytest_cmd += " --html={reports_dir}/report_serial.html"
 
     # Execute the pytest command
-    subprocess.run(cmd)
+    subprocess.run(pytest_cmd, shell=True)
+
 
 if __name__ == "__main__":
     config = get_config_data()
