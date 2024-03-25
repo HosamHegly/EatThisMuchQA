b3f1f67d2ae75048f6b5ff04506296e50d7cc592
HosamHegly <hossam_one@hotmail.com>
2024-03-26 01:29:04 +0200

    changed pytest version

--- parallel_test_runner_sample.py
+++ parallel_test_runner_sample.py
@@ -2,48 +2,34 @@ import os
 import subprocess
 from Utils.json_reader import get_config_data
 
-import os
-import subprocess
-from Utils.json_reader import get_config_data
-
 
 def run_pytest(parallel=False):
     # Load configuration
     config = get_config_data()
 
     ui_tests_path = "tests/api"
-    reports_dir = "reports"
+    reports_dir = "allure-results"
     os.makedirs(reports_dir, exist_ok=True)
 
     python_path = os.path.join("venv", "Scripts", "python.exe")
 
     # Base command using the virtual environment's Python
-    base_cmd = [python_path, "-m", "pytest", ui_tests_path]
-
-    html_report = os.path.join(reports_dir, "report.html")
+    base_cmd = [python_path, "-m", "pytest", ui_tests_path, "--alluredir", reports_dir]
 
     if parallel:
-        parallel_cmd = base_cmd + ["-n", "3", "-m", "not serial", f"--html={html_report}"]
-        try:
-            subprocess.run(parallel_cmd, check=True)
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
+        parallel_cmd = base_cmd + ["-n", "3", "-m", "not serial"]
+        subprocess.run(parallel_cmd)
 
+    # Run serial tests if there are any
     try:
-        serial_html_report = os.path.join(reports_dir, "report_serial.html")
-        serial_cmd = base_cmd + ["-m", "serial", f"--html={serial_html_report}"]
-        subprocess.run(serial_cmd, check=True)
+        serial_cmd = base_cmd + ["-m", "serial"]
+        subprocess.run(serial_cmd)
     except subprocess.CalledProcessError as e:
-        if e.returncode == 5:  # No tests were collected
-            print("No serial tests were found.")
-        else:
-            print(e.returncode)
-    else:
-        non_parallel_cmd = base_cmd + [f"--html={html_report}"]
-        try:
-            subprocess.run(non_parallel_cmd, check=True)
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
+        print(f"Error running serial tests: {e}")
+
+    # Generate the allure report after all tests have run
+    allure_report_dir = "allure-report"
+    subprocess.run(["allure", "generate", reports_dir, "-o", allure_report_dir, "--clean"], check=True)
 
 
 if __name__ == "__main__":
