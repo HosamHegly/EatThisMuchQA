6d478ed8f503cd335d6efdb99676aa57492d6933
HosamHegly <hossam_one@hotmail.com>
2024-03-27 21:58:25 +0200

    fixed runner

--- parallel_test_runner_sample.py
+++ parallel_test_runner_sample.py
@@ -10,15 +10,15 @@ from Utils.json_reader import get_config_data
 def run_pytest(parallel=False):
     # Load configuration
     config = get_config_data()
-
-    ui_tests_path = "tests/api"
+    ui_test_path = "test/ui/test_nuritional_target.py"
+    api_tests_path = "tests/api"
     reports_dir = "reports"
     os.makedirs(reports_dir, exist_ok=True)
 
     python_path = os.path.join("venv", "Scripts", "python.exe")
 
     # Base command using the virtual environment's Python
-    base_cmd = [python_path, "-m", "pytest", ui_tests_path]
+    base_cmd = [python_path, "-m", "pytest", ui_test_path,api_tests_path]
 
     html_report = os.path.join(reports_dir, "report.html")
 
