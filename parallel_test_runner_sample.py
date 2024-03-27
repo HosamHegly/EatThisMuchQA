e132d5979035222ef8aaa1df7a59f6378b9b57d7
HosamHegly <hossam_one@hotmail.com>
2024-03-27 17:34:33 +0200

    improved html report tests

--- parallel_test_runner_sample.py
+++ parallel_test_runner_sample.py
@@ -2,48 +2,41 @@ import os
 import subprocess
 from Utils.json_reader import get_config_data
 
-import os
-import subprocess
-from Utils.json_reader import get_config_data
-
-
 def run_pytest(parallel=False):
     # Load configuration
     config = get_config_data()
 
-    ui_tests_path = "tests/ui/test_food_addition.py"
-    reports_dir = "reports"
+    ui_tests_path = "tests/api"
+    reports_dir = "reports/allure-results"
     os.makedirs(reports_dir, exist_ok=True)
 
+    python_path = os.path.join("venv", "Scripts", "python.exe")
 
     # Base command using the virtual environment's Python
-    base_cmd = ["pytest", ui_tests_path]
+    base_cmd = [python_path, "-m", "pytest", ui_tests_path]
 
-    html_report = os.path.join(reports_dir, "report.html")
+    # Allure reports directory command
+    allure_cmd = ["--alluredir", reports_dir]
 
     if parallel:
-        parallel_cmd = base_cmd + ["-n", "3", "-m", "not serial", f"--html={html_report}"]
+        parallel_cmd = base_cmd + ["-n", "3", "-m", "not serial"] + allure_cmd
         try:
             subprocess.run(parallel_cmd, check=True)
         except subprocess.CalledProcessError as e:
-            print(f"Tests failed with return code {e.returncode}. Continuing the build...")
+            print(f"Parallel tests failed with return code {e.returncode}. Continuing the build...")
 
     try:
-        serial_html_report = os.path.join(reports_dir, "report_serial.html")
-        serial_cmd = base_cmd + ["-m", "serial", f"--html={serial_html_report}"]
+        serial_cmd = base_cmd + ["-m", "serial"] + allure_cmd
         subprocess.run(serial_cmd, check=True)
     except subprocess.CalledProcessError as e:
         if e.returncode == 5:  # No tests were collected
             print("No serial tests were found.")
         else:
-            print(e.returncode)
-    else:
-        non_parallel_cmd = base_cmd + [f"--html={html_report}"]
-        try:
-            subprocess.run(non_parallel_cmd, check=True)
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
+            print(f"Serial tests failed with return code {e.returncode}.")
 
+    # Optional: Generate an Allure report in a new directory
+    allure_report_dir = "reports/allure-report"
+    subprocess.run(["C:/npm/allure.cmd", "generate", reports_dir, "-o", allure_report_dir, "--clean"], check=True)
 
 if __name__ == "__main__":
     is_parallel = get_config_data().get("parallel", False)
